5ee1730b9f127ceb34f1f55f923e59abcb19bf0e
Tony Dinh <tony@tdinh.me>
2022-01-11 17:49:50 +0700

    Fix Package.swift again

--- Package.swift
+++ Package.swift
@@ -5,6 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftCron",
+    products: [
+    .library(
+      name: "SwiftCron",
+      targets: ["SwiftCron"]),
+    ],
     dependencies: [],
     targets: []
 )
